aa02a8ad041dd689f5649efc0665e52f5700d330
PANKAJ SHARMA <gaur.ps21@outlook.com>
2023-02-11 18:50:12 +0000

    finsl

---
+++ src/main/java/com/microservice/ELKStackex/ElkStackexApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.ELKStackex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkStackexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkStackexApplication.class, args);
+	}
+
+}
